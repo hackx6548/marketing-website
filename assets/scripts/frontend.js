5431bc696cd319517a6c754ad378d87b38ea42e9
pilar <sauleturme@gmail.com>
2018-11-06 16:28:34 +0100

    added font-family for the whole page and fixed the shadow of the brackets in the header

---
+++ assets/scripts/frontend.js
@@ -0,0 +1,9 @@
+let Typed = require('./typed.min.js');
+
+import Typed from "typed.js";
+
+let typedCursor = new Typed('#typed-cursor', {
+  strings: ["Learn digital skills with us to get the most fulfilling jobs."],
+  typeSpeed: 40
+});
+/*
