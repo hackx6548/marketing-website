1e7a77babd09eb90c11451b6227d28982b7724bf
Fakher Alhamad <fakher1281992@gmail.com>
2018-12-08 23:12:03 +0100

    Mailchimp with clean code finally

--- controllers/PagesController.js
+++ controllers/PagesController.js
@@ -1,5 +1,4 @@
 require('dotenv').config({ path: __dirname + '/../.env' });
-const request = require("request");
 const Page = require("../models/page");
 
 module.exports.getPages = async function (req, res) {
@@ -22,61 +21,4 @@ module.exports.getSinglePage = async (req, res) => {
   } catch (err) {
     console.log(err);
   }
-}
-
-module.exports.newsletterSignup = function (req, res) {
-  const { email } = req.body;
-
-  // Make sure fields are filled
-  if (!email) {
-    return res.json({
-      code: 422,
-      message: "No valid email address given!"
-    });
-  }
-
-  // Construct req data
-  const data = {
-    members: [
-      {
-        email_address: email,
-        status: 'subscribed'
-        // merge_fields: {}
-      }
-    ]
-  };
-
-  const postData = JSON.stringify(data);
-
-  const options = {
-    url: 'https://us18.api.mailchimp.com/3.0/lists/343514d9d9',
-    method: 'POST',
-    headers: {
-      Authorization: 'auth 182808b0b5516cdd6b5a99ca2d3b7a49-us18'
-    },
-    body: postData
-  };
-
-  request(options, (err, response) => {
-    if (err) {
-      return res.json({
-        code: response.statusCode,
-        message: error.message
-      });
-    } else {
-      const json = JSON.parse(response.body);
-
-      if (response.statusCode === 200 && json.errors.length === 0) {
-        return res.json({
-          code: 200,
-          message: "Successfully subscribed to the newsletter!"
-        });
-      } else {
-        return res.json({
-          code: json.errors ? 422 : response.statusCode,
-          message: "error"
-        }, 422);
-      }
-    }
-  });
 }
